d4d08188124aa3059657cc0afcb610523842c5bc
Varya <varya.example.com>
2025-10-03 17:12:27 +0300

    login screen added

--- lib/main.dart
+++ lib/main.dart
@@ -6,117 +6,155 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ScreenSwitcher(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ScreenSwitcher extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ScreenSwitcherState createState() => _ScreenSwitcherState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ScreenSwitcherState extends State<ScreenSwitcher> {
+  int current = 0;
 
-  void _incrementCounter() {
+  String email = '';
+  String login = '';
+  String password = '';
+  String loadStatus = 'нет данных';
+
+  void saveLoginData(String l, String p) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      login = l;
+      password = p;
     });
   }
 
+  void saveSignInData(String eml, String logn, String passwd) {
+    setState(() {
+      email = eml;
+      login = logn;
+      password = passwd;
+    });
+  }
+
+  void loadData(int index) {
+    setState(() {
+      loadStatus = 'данные загружены';
+    });
+  }
+
+  List<Widget> get screens => [
+    LoginScreen(onSave: saveLoginData),
+    // SignInScreen(onSave: saveSignInData),
+    // DisplayScreen(login: login, password: password),
+    // LoadDataScreen(status: loadStatus, onLoad: () => loadData(0)),
+    // AboutAppScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Практика 3'),
+        backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Expanded(child: screens[current]),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => setState(() => current = 0),
+                  child: Text('Авторизация'),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => current = 1),
+                  child: Text('Регистрация'),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => current = 2),
+                  child: Text('Пользователь'),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => current = 3),
+                  child: Text('Загрузка'),
+                ),
+                ElevatedButton(
+                  onPressed: () => setState(() => current = 4),
+                  child: Text('О приложении'),
+                ),
+              ]
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class LoginScreen extends StatefulWidget {
+  final void Function(String, String) onSave;
+  const LoginScreen({required this.onSave});
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController loginController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    loginController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 16),
+        Text("Авторизация", style: TextStyle(fontSize: 32)),
+        SizedBox(height: 64),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: TextField(
+            controller: loginController,
+            decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Логин'),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: TextField(
+            controller: passwordController,
+            decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Пароль'),
+            obscureText: true,
+          ),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            widget.onSave(loginController.text, passwordController.text);
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Данные сохранены')));
+          },
+          child: Text('Сохранить'),
+        ),
+      ],
+    );
+  }
+}
+
